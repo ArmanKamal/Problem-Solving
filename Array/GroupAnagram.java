c498a744544336da0ace6249ac3a30661f791452
ArmanKamal <armankamal2421@gmail.com>
2023-12-21 23:20:51 -0600

    Group Anagram O(n) solution

--- Array/GroupAnagram.java
+++ Array/GroupAnagram.java
@@ -3,16 +3,38 @@ import java.util.*;
 public class GroupAnagram {
     public List<List<String>> groupAnagrams(String[] strs) {
         /* Time - O(n)logn  Space - O(1) */
-        HashMap<String, List<String>> map= new HashMap();
-        for(String s : strs){
-            char[] sorted_string = s.toCharArray();
-            Arrays.sort(sorted_string);
-            String newString = new String(sorted_string);
-            if(!map.containsKey(newString)){
-                map.put(newString, new ArrayList<>());
+//        HashMap<String, List<String>> map= new HashMap();
+//        for(String s : strs){
+//            char[] sorted_string = s.toCharArray();
+//            Arrays.sort(sorted_string);
+//            String newString = new String(sorted_string);
+//            if(!map.containsKey(newString)){
+//                map.put(newString, new ArrayList<>());
+//            }
+//            map.get(newString).add(s);
+//        }
+//        return new ArrayList(map.values());
+
+        HashMap<String, List<String>> mappedResult = new HashMap<>();
+
+        for(String s: strs){
+            int[] count = new int[26];
+            for(Character c: s.toCharArray()){
+                count[c-'a']++;
             }
-            map.get(newString).add(s);
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<26;i++){
+                sb.append(count[i]);
+                sb.append("#");
+            }
+            String finalString = sb.toString();
+            if(!mappedResult.containsKey(finalString)){
+                mappedResult.put(finalString, new ArrayList<>());
+            }
+            mappedResult.get(finalString).add(s);
         }
-        return new ArrayList(map.values());
+        return new ArrayList<>(mappedResult.values());
+
+
     }
 }
