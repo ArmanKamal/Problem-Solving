6d858ca7690e935fdb8a150ebaaff3b72efefe7c
ArmanKamal <armankamal2421@gmail.com>
2024-01-05 21:54:39 -0600

    Dailiy Temperature O(n) solution stack

--- Stack/DailyTemperatures.java
+++ Stack/DailyTemperatures.java
@@ -1,14 +1,29 @@
 package Stack;
 
+import java.util.Stack;
+
 public class DailyTemperatures {
     // 72 73 69
     public static void main(String[] args){
         DailyTemperatures dT = new DailyTemperatures();
-        int[] result = dT.dailyTemperatures(new int[]{73,74,75,71,69,72,76,73});
+        int[] result = dT.dailyTemperatuesWithStack(new int[]{73,74,75,71,69,72,76,73});
         for(int r: result){
             System.out.print(r+" ");
         }
 
+    }
+
+    public int[] dailyTemperatuesWithStack(int[] temperatures){
+        int[] result = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<temperatures.length;i++){
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+                int insertIdx = stack.pop();
+                result[insertIdx] = i-insertIdx;
+            }
+            stack.push(i);
+        }
+        return result;
     }
 
     public int[] dailyTemperatures(int[] temperatures) {
