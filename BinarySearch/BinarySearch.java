03ce5ccad00ddf63d5d8d9760a0a6315ac6dcab6
ArmanKamal <armankamal2421@gmail.com>
2024-01-07 03:32:24 -0600

    Binary Search solution

---
+++ BinarySearch/BinarySearch.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+public class BinarySearch {
+    public static void main(String[] args){
+        BinarySearch bs = new BinarySearch();
+        System.out.println(bs.search(new int[]{-1,0,3,5,9,12},9));
+    }
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<right){
+            int mid = (left+right)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(target>nums[mid]){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return -1;
+    }
+}
