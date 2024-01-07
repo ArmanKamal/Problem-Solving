1c201f7db9b4f582898b3d89c4ed81ddf30847b3
ArmanKamal <armankamal2421@gmail.com>
2024-01-07 02:19:22 -0600

    fixed car fleet

--- Stack/CarFleet.java
+++ Stack/CarFleet.java
@@ -1,33 +1,36 @@
 package Stack;
 
 import java.util.Arrays;
+import java.util.Stack;
 
 public class CarFleet {
-    public int carFleet(int target, int[] position, int[] speed){
+    public int carFleet(int target, int[] position, int[] speed) {
         CarNode[] carNodes = new CarNode[position.length];
-        for(int i=0;i<position.length;i++){
-            carNodes[i].position = position[i];
-            carNodes[i].speed = speed[i];
+        for (int i = 0; i < position.length; i++) {
+            carNodes[i] = new CarNode(position[i], speed[i]);
         }
-        Arrays.sort(carNodes, (a,b) -> b.position-a.position);
+        Arrays.sort(carNodes, (a, b) -> b.position - a.position);
         int fleets = 0;
-        int lastTime = target-carNodes[0].position/carNodes[0].speed;
-        for(int i=1;i<carNodes.length;i++){
-            int newTime = target-carNodes[i].position/carNodes[i].speed;
-            if(newTime>lastTime){
+        double lastTime = 0;
+        for (int i = 0; i < carNodes.length; i++) {
+            double newTime = (double) (target - carNodes[i].position) / (double) carNodes[i].speed;
+            if (newTime > lastTime) {
                 fleets++;
                 lastTime = newTime;
             }
+
         }
         return fleets;
     }
 
-     public class CarNode{
+    public class CarNode {
         int position;
         int speed;
-        CarNode(int position, int speed){
+
+        CarNode(int position, int speed) {
             this.position = position;
             this.speed = speed;
         }
+
     }
 }
