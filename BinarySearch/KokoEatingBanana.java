02475a853a60b01d63f285d5ed4f4f8ddeca026f
ArmanKamal <armankamal2421@gmail.com>
2024-01-08 19:56:08 -0600

    Optimal Solution of Koko

--- BinarySearch/KokoEatingBanana.java
+++ BinarySearch/KokoEatingBanana.java
@@ -23,4 +23,19 @@ public class KokoEatingBanana {
             else speed++;
         }
     }
+    public int minEatingSpeedEfficientSolution(int[] piles, int h){
+        int left = 1;
+        int right = Arrays.stream(piles).max().getAsInt();
+
+        while(left<right){
+            int mid = left+(right-left)/2;
+            int total = 0;
+            for(int pile: piles){
+                total += Math.ceil((double) pile/ mid);
+            }
+            if(total<=h) right = mid;
+            else left = mid+1;
+        }
+        return right;
+    }
 }
